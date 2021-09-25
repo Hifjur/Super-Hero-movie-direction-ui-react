0bbf98bdf13b8420893897dbdac78e934542aca6
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-09-26 02:37:37 +0600

    JSON file done and header done

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <h1>Make The Best Concert</h1>
+            <p>Legendary singers from all aound the world unites to make an epic show</p>
+            <h3>Total Budget: <span className="budget-color">100 Million</span></h3>
+        </div>
+    );
+};
+
+export default Header;
